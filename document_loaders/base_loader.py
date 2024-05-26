021adeee74dd59428a3906fd455e9fe107dc9aa4
이준영 <fgh33678@gmail.com>
2024-05-26 16:54:32 +0900

    BaseLoader load_and_split 추가

--- document_loaders/base_loader.py
+++ document_loaders/base_loader.py
@@ -3,3 +3,13 @@ from abc import ABC
 class BaseLoader(ABC):
     def load(self):
         pass
+
+    def load_and_split(self):
+        try:
+            from LangChainJ.text_splitters.character_text_splitter import CharacterTextSplitter
+        except ImportError as error:
+            raise ImportError(f"{error}")
+        
+        text_splitter = CharacterTextSplitter()
+        doc = self.load()
+        return text_splitter.split_text(doc)
