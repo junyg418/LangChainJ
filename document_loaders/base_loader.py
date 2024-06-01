f3ae6d87ec35415b70c0ffb76e93db0a573710da
이준영 <fgh33678@gmail.com>
2024-06-01 19:03:23 +0900

    Changed the default splitter of BaseLoader's load_and_split func to RecursiveCharacterTextSplitter.

--- document_loaders/base_loader.py
+++ document_loaders/base_loader.py
@@ -7,10 +7,10 @@ class BaseLoader(ABC):
 
     def load_and_split(self):
         try:
-            from LangChainJ.text_splitters.character_text_splitter import CharacterTextSplitter
+            from LangChainJ.text_splitters import RecursiveCharacterTextSplitter
         except ImportError as error:
             raise ImportError(f"{error}")
         
-        text_splitter = CharacterTextSplitter()
+        text_splitter = RecursiveCharacterTextSplitter()
         doc = self.load()
         return text_splitter.split_text(doc.page_content)
